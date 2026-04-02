1ff5c6c09731382e24f1979656685ae67ffde998
evan0960 <eluo0960@gmail.com>
2026-04-01 22:40:13 -0400

    a
    a

---
+++ script.js
@@ -0,0 +1,18 @@
+var button = document.getElementById("button");
+var image = document.getElementById("img1");
+var numclicks = 0;
+button.onclick(function(){
+    if (numclicks < 2){
+        var randomnum = Math.floor(Math.random() * 3);
+        if (randomnum == 0) {
+            image.setAttribute("src","55098982592_17a11a6e82_o.jpg");
+        } else if (randomnum == 1) {
+            image.setAttribute("src","IMG_4418.png");
+        } else if (randomnum == 2) {
+            image.setAttribute("src","IMG_4423.png");
+        }
+        numclicks++;
+    } else {
+        window.location.href = "http://youtube.com/watch?v=xMHJGd3wwZk";
+    }
+})
